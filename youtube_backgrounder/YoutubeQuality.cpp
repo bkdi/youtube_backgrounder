77cb3ff46ba0a553ff97a89e961e3854be5f3646
Dzik <bkdi@o2.pl>
2016-11-20 22:07:39 +0100

    remove unsupported itags by mediaelement

--- youtube_backgrounder/YoutubeQuality.cpp
+++ youtube_backgrounder/YoutubeQuality.cpp
@@ -12,7 +12,6 @@ const std::vector<YoutubeQualityItag> youtube_backgrounder::sortedNonDASH_2DVide
 	YoutubeQualityItag::Non_DASH_2D_3GP_Low,	
 	YoutubeQualityItag::Non_DASH_2D_FLV_Low,	
 	YoutubeQualityItag::Non_DASH_2D_MP4_Medium,	
-	YoutubeQualityItag::Non_DASH_2D_WebM_Medium,	
 	YoutubeQualityItag::Non_DASH_2D_MP4_High		
 };
 
@@ -20,7 +19,6 @@ const std::vector<YoutubeQualityItag> youtube_backgrounder::sortedDASH_AudioQual
 {
 	YoutubeQualityItag::DASH_Audio_WebM_VeryLow,
 	YoutubeQualityItag::DASH_Audio_WebM_Low,		
-	YoutubeQualityItag::DASH_Audio_WebM_Medium,	
 	YoutubeQualityItag::DASH_Audio_M4A_Medium,	
 	YoutubeQualityItag::DASH_Audio_WebM_High		
 };
